cdf0928c03daf8f87db6797a7f76e0abf5bf539b
David Martinho <davidmartinho@gmail.com>
2013-10-30 14:03:48 +0000

    Attempt to improve map from main page

--- map/map.js
+++ map/map.js
@@ -1,10 +1,53 @@
-var map;
-var lisbon = new google.maps.LatLng(38.76265, -9.21753);
-var infoBubble;
+(function(window) {
 
-var MY_MAPTYPE_ID = 'custom_style';
+  var schools = [
+    { name: "Instituto Superior Técnico",
+      url: "http://ist.utl.pt",
+      imagePin: "map/map_pin.png",
+      logo: "logo_school_ist.png",
+      location: [ 38.73763 , -9.13910 ],
+      z: 6,
+      numUsers: 65148
+    },
+    { name: "ISEG",
+      url: "http://aquila.iseg.utl.pt",
+      imagePin: "map/map_pin.png",
+      logo: "logo_school_iseg.png",
+      location: [ 38.71244 , -9.15496 ],
+      z: 5,
+      numUsers: 3333 },
 
-function initialize() {
+    { name: "ISA",
+      url: "http://www.isa.utl.pt",
+      imagePin: "map/map_pin.png",
+      logo: "logo_school_isa.png",
+      location: [ 38.7107,  -9.1814 ], 
+      z: 4,
+      numUsers: 9296 },
+
+    { name: "FA",
+      url: "http://www.fa.utl.pt",
+      imagePin: "map/map_pin.png",
+      logo: "logo_school_fa.png",
+      location: [ 38.7144,  -9.1917 ], 
+      z: 3,
+      numUsers: 666 },
+
+    { name: "ESESJC",
+      url: "http://www.esesjcluny.pt",
+      imagePin: "map/map_pin.png",
+      logo: "logo_school_esesjc.png",
+      location: [ 32.66678 , -16.90475],
+      z: 2,
+      numUsers: 1447 },
+
+    { name: "ISCTE",
+      url: "http://www.iscte-iul.pt",
+      imagePin: "map/map_pin.png",
+      logo: "logo_school_iscte.png",
+      location: [ 38.74803 , -9.15338 ],
+      z: 1,
+      numUsers: 52488 }];
 
   var featureOpts = [
     {
@@ -12,237 +55,166 @@ function initialize() {
         { visibility: 'simplified' },
         { gamma: 0.7 },
         { weight: 0.3 },
-        { saturation: -100 }
-      ]
-    },
+        { saturation: -100 }]},
     {
       elementType: 'labels',
       stylers: [
-        { visibility: 'off' }
-      ]
-    },
+        { visibility: 'off' }]},
     {
       featureType: 'water',
       stylers: [
-        { color: '#eeeeee' }
-      ]
-    }
+        { color: '#eeeeee' }]}
   ];
 
+  var FENIXEDU_MAPTYPE_ID = 'fenixedu_style';
+
   var mapOptions = {
-      zoom: 5,
-      scrollwheel: false,
-      center: lisbon,
-      mapTypeControl: false,
-      mapTypeControlOptions: {
-        mapTypeId: [MY_MAPTYPE_ID]
-      },
-      zoomControl: true,
-      zoomControlOptions: {
-        style: google.maps.ZoomControlStyle.SMALL
-      },
-      mapTypeId: MY_MAPTYPE_ID
+    zoom: 5,
+    scrollwheel: false,
+    streetViewControl: false,
+    mapTypeControl: false,
+    mapTypeControlOptions: {
+      mapTypeId: [FENIXEDU_MAPTYPE_ID]
+    },
+    zoomControl: true,
+    zoomControlOptions: {
+      style: google.maps.ZoomControlStyle.SMALL
+    },
+    mapTypeId: FENIXEDU_MAPTYPE_ID
   };
 
-  map = new google.maps.Map(document.getElementById('map'),
-      mapOptions);
-
-  var styledMapOptions = {
-      name: 'Custom Style'
+  var subMapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(32.66678 , -16.90475),
+    scrollwheel: false,
+    panControl: false,
+    streetViewControl: false,
+    mapTypeControl: false,
+    mapTypeControlOptions: {
+      mapTypeId: [FENIXEDU_MAPTYPE_ID]
+    },
+    zoomControl: false,
+    mapTypeId: FENIXEDU_MAPTYPE_ID
   };
-
-  var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
-
-  map.mapTypes.set(MY_MAPTYPE_ID, customMapType);
-
-
-  // Markers
-
-  var image = 'map/map_pin.png';
-
-  var istLatLng = new google.maps.LatLng(38.73763, -9.13910); 
-  var isegLatLng = new google.maps.LatLng(38.71244, -9.15496);
-  var isaLatLng = new google.maps.LatLng(38.7107, -9.1814);
-  var faLatLng = new google.maps.LatLng(38.7144, -9.1917);
-  var esesjcLatLng = new google.maps.LatLng(32.66678, -16.90475);
-  var iscteLatLng = new google.maps.LatLng(38.74803, -9.15338);
-
-  var istMarker = new google.maps.Marker({
-      position: istLatLng,
-      map: map,
-      icon: image,
-      title: 'Instituto Superior Técnico'
-  });
-
-  var isegMarker = new google.maps.Marker({
-      position: isegLatLng,
-      map: map,
-      icon: image,
-      title: 'Lisboa School of Economics & Management'
-  });
-
-  var isaMarker = new google.maps.Marker({
-      position: isaLatLng,
-      map: map,
-      icon: image,
-      title: 'Instituto Superior de Agronomia'
-  });
-
-  var faMarker = new google.maps.Marker({
-      position: faLatLng,
-      map: map,
-      icon: image,
-      title: 'Faculdade de Arquitetura de Lisboa'
-  });
-
-  var esesjcMarker = new google.maps.Marker({
-      position: esesjcLatLng,
-      map: map,
-      icon: image,
-      title: 'São José de Cluny Nursing School'
-  });
-
-  var iscteMarker = new google.maps.Marker({
-      position: iscteLatLng,
-      map: map,
-      icon: image,
-      title: 'ISCTE - University Institute of Lisbon'
-  });
-
-  var bounds = new google.maps.LatLngBounds(isaLatLng, iscteLatLng);
-      map.fitBounds(bounds);
-
-  // InfoBubbles
-
-  var istContentString = '<a href="http://www.ist.utl.pt">'+
-      '<div class="schoolLogo">'+
-      '<img src="map/logos/logo_school_ist.png" alt="Instituto Superior Técnico">'+
-      '</div>'+
-      '<hr />'+
-      '<span>65148 Users</span>'+
-      '</a>';
-
-  var isegContentString = '<a href="http://aquila.iseg.utl.pt/">'+
-      '<div class="schoolLogo">'+
-      '<img src="map/logos/logo_school_iseg.png" alt="Lisboa School of Economics & Management">'+
-      '</div>'+
-      '<hr />'+
-      '<span>3333 Users</span>'+
-      '</a>';
-
-  var isaContentString = '<a href="http://www.isa.utl.pt">'+
-      '<div class="schoolLogo">'+
-      '<img src="map/logos/logo_school_isa.png" alt="Instituto Superior de Agronomia">'+
-      '</div>'+
-      '<hr />'+
-      '<span>9296 Users</span>'+
-      '</a>';
-
-  var faContentString = '<a href="http://www.fa.utl.pt/">'+
-      '<div class="schoolLogo">'+
-      '<img src="map/logos/logo_school_fa.png" alt="Faculdade de Arquitetura de Lisboa" width="48" height="auto">'+
-      '</div>'+
-      '<hr />'+
-      '<span>666 Users</span>'+
-      '</a>';
-
-  var esesjcContentString = '<a href="http://www.esesjcluny.pt/">'+
-      '<div class="schoolLogo">'+
-      '<img src="map/logos/logo_school_esesjc.png" alt="São José de Cluny Nursing School">'+
-      '</div>'+
-      '<hr />'+
-      '<span>1447 Users</span>'+
-      '</a>';
-
-  var iscteContentString = '<a href="http://www.iscte-iul.pt">'+
-      '<div class="schoolLogo">'+
-      '<img src="map/logos/logo_school_iscte.png" alt="ISCTE - University Institute of Lisbon">'+
-      '</div>'+
-      '<hr />'+
-      '<span>52488 Users</span>'+
-      '</a>';
 
   var bubbleOptions = {
-      map: map,
-      shadowStyle: 0,
-      padding: 12,
-      maxWidth: 180,
-      maxHeight: 100,
-      backgroundColor: '#19accd',
-      borderRadius: 0,
-      borderWidth: 0,
-      disableAutoPan: true,
-      hideCloseButton: true,
-      backgroundClassName: 'bubbleContent',
-      arrowPosition: 50,
-      arrowSize: 10,
-      arrowStyle: 0
+    map: map,
+    shadowStyle: 0,
+    padding: 12,
+    maxWidth: 180,
+    maxHeight: 100,
+    backgroundColor: '#19accd',
+    borderRadius: 0,
+    borderWidth: 0,
+    disableAutoPan: true,
+    hideCloseButton: true,
+    backgroundClassName: 'bubbleContent',
+    arrowPosition: 50,
+    arrowSize: 10,
+    arrowStyle: 0
   };
 
-  var istBubble = new InfoBubble(
-    merge_options({
-      position: istLatLng,
-      content: istContentString
-  },bubbleOptions));
+  var fenixEduMapStyle = new google.maps.StyledMapType(featureOpts, { name: 'FenixEdu Style'});
 
-  var isegBubble = new InfoBubble(
-    merge_options({
-      position: isegLatLng,
-      content: isegContentString
-  },bubbleOptions));
+  var imagePin = 'map/map_pin.png';
 
-  var isaBubble = new InfoBubble(
-    merge_options({
-      position: isaLatLng,
-      content: isaContentString
-  },bubbleOptions));
+  var map;
 
-  var faBubble = new InfoBubble(
-    merge_options({
-      position: faLatLng,
-      content: faContentString
-  },bubbleOptions));
+  var markers = window.markers = [];
+  var bubbles = [];
 
-  var esesjcBubble = new InfoBubble(
-    merge_options({
-      position: esesjcLatLng,
-      content: esesjcContentString
-  },bubbleOptions));
-
-  var iscteBubble = new InfoBubble(
-    merge_options({
-      position: iscteLatLng,
-      content: iscteContentString
-  },bubbleOptions));
-
-//google.maps.event.addListener(istMarker, 'click', function() {
-  istBubble.open(map, istMarker);
-//});
-//google.maps.event.addListener(isegMarker, 'click', function() {
-  isegBubble.open(map, isegMarker);
-//});
-//google.maps.event.addListener(isaMarker, 'click', function() {
-  isaBubble.open(map, isaMarker);
-//});
-//google.maps.event.addListener(faMarker, 'click', function() {
-  faBubble.open(map, faMarker);
-//});
-//google.maps.event.addListener(esesjcMarker, 'click', function() {
-  esesjcBubble.open(map, esesjcMarker);
-//});
-//google.maps.event.addListener(iscteMarker, 'click', function() {
-  iscteBubble.open(map, iscteMarker);
-//});
-
-}
-
-// Functions Aux.
-
-function merge_options(obj1,obj2){
+  var mergeObjects = function(obj1, obj2) {
     var obj3 = {};
     for (var attrname in obj1) { obj3[attrname] = obj1[attrname]; }
     for (var attrname in obj2) { obj3[attrname] = obj2[attrname]; }
     return obj3;
-}
+  };
 
-google.maps.event.addDomListener(window, 'load', initialize);
+  var populateSchoolMarkers = function(map, submap, schools) {
+    for(var i = 0; i < schools.length; i++) {
+      var school = schools[i];
+      var location = school["location"];
+      var imagePin = school["imagePin"];
+      var latLon = new google.maps.LatLng(location[0], location[1]);
+      var contentString = '<a href='+school["url"]+'><div class="schoolLogo"><img src="map/logos/'+school["logo"]+'" alt="'+school["name"]+'"></div><hr/><span>'+school["numUsers"]+'</span></a>';
+
+      var marker = new google.maps.Marker({
+        position: latLon,
+        map: map,
+        icon: imagePin,
+        title: school["name"],
+        zIndex: school["z"]
+      });
+
+      var subMarker = new google.maps.Marker({
+        position: latLon,
+        map: submap,
+        icon: imagePin,
+        title: school["name"]
+      });
+
+      var bubble = new InfoBubble(
+        mergeObjects({
+          position: latLon,
+          content: contentString
+        }, bubbleOptions));
+
+      bubble.open(map, marker);
+
+      markers.push(marker);
+      bubbles.push(bubble);
+
+      google.maps.event.addListener(marker, 'click', function() {
+        var bubble = bubbles[markers.indexOf(this)];
+        bubble.open();          
+      });
+
+      google.maps.event.addListener(subMarker, 'click', function() {
+        map.setCenter(this.getPosition());
+        map.setZoom(11);
+      });
+
+    }
+  };
+
+  var onZoomChange = function() {
+    var zoomLevel = map.getZoom();
+    if(zoomLevel >= 14) {
+      for(var i = 0; i < bubbles.length; i++) {
+        var bubble = bubbles[i];
+        bubble.open();
+      }
+    } else {
+      for(var i = 0; i < bubbles.length; i++) {
+        var bubble = bubbles[i];
+        bubble.close();
+      }
+    }
+    
+  };
+
+  var initialize = function() {
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    
+    google.maps.event.addListener(map, 'zoom_changed', onZoomChange);
+    map.mapTypes.set(FENIXEDU_MAPTYPE_ID, fenixEduMapStyle);
+    map.setMapTypeId(FENIXEDU_MAPTYPE_ID);
+
+    var bounds = new google.maps.LatLngBounds(new google.maps.LatLng(38.7107, -9.15338), new google.maps.LatLng(38.74803, -9.15338));
+    map.setCenter(bounds.getCenter());
+    map.fitBounds(bounds);
+
+    var subMap = window.subMap = new google.maps.Map(document.getElementById('submap'), subMapOptions);
+    subMap.mapTypes.set(FENIXEDU_MAPTYPE_ID, fenixEduMapStyle);
+    subMap.setMapTypeId(FENIXEDU_MAPTYPE_ID);
+
+    populateSchoolMarkers(map, subMap, schools);
+
+  };
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+
+})(this);
+
+
+
